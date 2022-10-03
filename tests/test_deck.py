08094a6af2a1865935e8fa91ab0b272e8ef6499f
ziky5 <zikan.p@gmail.com>
2022-10-03 15:47:10 +0000

    Lesson 3: Card and Deck

---
+++ tests/test_deck.py
@@ -0,0 +1,37 @@
+from ultimatni_prirucka.deck import Deck, Card
+
+from random import shuffle
+
+
+def test_create_card():
+    Card('barva', 'hodnota')
+
+
+def test_create_deck():
+    Deck()
+
+
+def test_deck_has_52_cards():
+    deck = Deck()
+    assert len(deck) == 52
+
+
+def test_can_be_shuffled():
+    deck = Deck()
+    shuffle(deck)
+
+
+def test_look_at_card():
+    deck = Deck()
+    card = deck[0]
+    assert card == Card("diamond", "2")
+
+
+def test_look_at_cards():
+    deck = Deck()
+    first_three_cards = deck[0:3]
+
+
+def test_card_is_immutable():
+    card = Card("b", "h")
+    card.colour = "b2"  # TODO: improve
